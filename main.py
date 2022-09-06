c6e08cc337e45e8cbc81306258de824c1860a2a5
Harrison Keating <harrisonrkeating@gmail.com>
2022-09-06 18:21:13 -0400

    Revamp using separate functions for each command, argparse, and relative file paths

--- main.py
+++ main.py
@@ -4,55 +4,188 @@ import urllib.request
 import shutil
 import os
 import random
+import argparse
+
+def printGuilds(guilds):
+    print("This bot is a member of the following servers:")
+    for g in guilds:
+        print("{} : {}".format(g.id, g))
+
+def printCategories(guild):
+    print(guild)
+    print("------")
+    for c in guild.categories:
+        print("{} : {}".format(c.id, c))
+    print()
+
+def printChannels(category):
+    print(category)
+    print("------")
+    for chan in category.channels:
+        if (chan.type == discord.ChannelType.text):
+            print("{} : {}".format(chan.id, chan))
+    print()
+
+async def backupGuild(guild):
+    print("backing up guild {} : {}".format(guild.id, guild))
+
+    folderName = os.path.join(os.getcwd(), str(guild.id))
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
+
+    f = open(folderName+"/index.htm", 'w', encoding="utf-8")
+    f.write("<h1>"+guild.name+"</h1>\n<ul>\n")
+
+    for category in guild.categories:
+        f.write("<li><a href='{}'>{}</a></li>\n<ul>\n".format(str(category.id)+"/index.htm", category.name))
+        for channel in category.channels:
+            f.write("<li><a href='{}'>{}</a></li>\n".format(str(category.id)+"/"+str(channel.id)+"/index.htm", channel.name))
+        f.write("</ul>\n")
+        f.close()
+        f = open(folderName+"/index.htm", 'a',  encoding="utf-8")
+        await backupCategory(category)
+
+    f.write("</ul>")
+    f.close()
+    return
+
+async def backupCategory(category):
+    print("--- backing up category {} : {}".format(category.id, category))
+
+    rootFolder = os.path.join(os.getcwd(), str(category.guild.id))
+    folderName = rootFolder+"/"+str(category.id)
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
+
+    f = open(folderName+"/index.htm", 'w', encoding="utf-8")
+    f.write("<h1><a href='../index.htm'>{}</a> / {} </h1><ul>\n".format(category.guild.name, category.name))
+
+    for channel in category.channels:
+        if channel.type == discord.ChannelType.text:
+            f.write("<li><a href='{}'>{}</a></li>".format(str(channel.id)+"/index.htm", channel.name))
+            await backupChannel(channel)
+
+    f.write("</ul>")
+    f.close()
+    return
+
+async def backupChannel(channel):
+    print("------ backing up channel {} : {}".format(channel.id, channel))
+
+    rootFolder = os.path.join(os.getcwd(), str(channel.guild.id))
+    folderName = rootFolder+"/"+str(channel.category.id)+"/"+str(channel.id)
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
+
+    f = open(folderName+"/index.htm", 'w', encoding="utf-8")
+
+    f.write("<h1>")
+    f.write("<a href='../../index.htm'>{}</a> / ".format(channel.guild.name))
+    f.write("<a href='../index.htm'>{}</a> / ".format(channel.category.name))
+    f.write("{}</h1>".format(channel.name))
+
+
+    srcName = os.path.join(folderName, "src")
+    if not os.path.exists(srcName):
+        os.makedirs(srcName)
+
+    out = []
+    async for m in channel.history(limit=None):
+        out.append('<p>{}: {}</p>\n'.format(m.author.name, m.content))
+
+        for a in m.attachments:
+            r = requests.get(a.url, stream = True)
+            file_name = a.url.split('/')[-1]
+            ext = file_name.split('.')[-1]
+
+            # if the file name already exists, append a random number
+            # from 0-9 until the file does not exist
+            fullPath = os.path.join(srcName, file_name)
+            while os.path.exists(fullPath):
+                i = file_name.rfind('.')
+                file_name = file_name[:i] + str(random.randint(0,9)) + file_name[i:]
+                fullPath = os.path.join(srcName, file_name)
+
+            with open(folderName+"/src/"+file_name,'wb') as out_file:
+                shutil.copyfileobj(r.raw, out_file)
+            print(a.url)
+            if (ext == 'png' or ext == 'jpg' or ext == 'jpeg' or ext == 'gif'):
+                out.append('<img src="src/'+file_name+'"></img>\n')
+            else:
+                out.append(a.url+"\n")
+    for o in out[::-1]:
+        f.write(o)
+    f.close()
+
+    return
 
 class MyClient(discord.Client):
-
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        for g in self.guilds:
-            folderName = os.path.join(os.getcwd(), g.name)
-            if not os.path.exists(folderName):
-                os.makedirs(folderName)
-            srcName = os.path.join(folderName, "src")
-            if not os.path.exists(srcName):
-                os.makedirs(srcName)
-            for c in g.channels:
-                if (c.type == discord.ChannelType.text):
-                    print("----- "+c.name+" -----")
-                    f = open(folderName+"/"+c.name+".htm", 'w')
-                    f.write("<h1>"+c.name+"</h1>\n")
-                    thisName = os.path.join(srcName, c.name)
-                    if not os.path.exists(thisName):
-                        os.makedirs(thisName)
-                    messages = await c.history(limit=None).flatten()
-                    messages = messages[::-1]
-                    for m in messages:
-                        f.write('<p>{0.author}: {0.content}</p>\n'.format(m))
-                        for a in m.attachments:
-                            r = requests.get(a.url, stream = True)
-                            file_name = a.url.split('/')[-1]
-                            ext = file_name.split('.')[-1]
-
-                            # if the file name already exists, append a random number
-                            # from 0-9 until the file does not exist
-                            fullPath = os.path.join(thisName, file_name)
-                            while os.path.exists(fullPath):
-                                i = file_name.rfind('.')
-                                file_name = file_name[:i] + str(random.randint(0,9)) + file_name[i:]
-                                fullPath = os.path.join(thisName, file_name)
-
-                            with open(thisName+"/"+file_name,'wb') as out_file:
-                                shutil.copyfileobj(r.raw, out_file)
-                            print(a.url)
-                            print(ext)
-                            if (ext == 'png' or ext == 'jpg' or ext == 'jpeg' or ext == 'gif'):
-                                print("made it!")
-                                f.write('<img src="src/'+c.name+"/"+file_name+'"></img>\n')
-                            else:
-                                f.write(a.url+"\n")
+        print('Successfully authenticated as {0}'.format(self.user))
+        if args.show:
+            if not args.guild:
+                printGuilds(self.guilds)
+            elif not args.category:
+                g = self.get_guild(args.guild)
+                if g:
+                    printCategories(g)
+                else:
+                    print("Guild does not exist")
+            elif not args.channel:
+                g = self.get_guild(args.guild)
+                if g:
+                    c = g.get_channel(args.category)
+                    if c:
+                        printChannels(c)
+                    else:
+                        print("Channel does not exist")
+                else:
+                    print("Guild does not exist")
+        else:
+            if not args.guild:
+                print("Please specify a guild")
+                quit()
+            elif not args.category:
+                g = self.get_guild(args.guild)
+                if g:
+                    await backupGuild(g)
+                else:
+                    print("Guild does not exist")
+            elif not args.channel:
+                g = self.get_guild(args.guild)
+                if g:
+                    c = g.get_channel(args.category)
+                    if c:
+                        await backupCategory(c)
+                    else:
+                        print("Channel does not exist")
+                else:
+                    print("Guild does not exist")
+            else:
+                g = self.get_guild(args.guild)
+                if g:
+                    c = g.get_channel(args.channel)
+                    if c:
+                        await backupChannel(c)
+                    else:
+                        print("Channel does not exist")
+                else:
+                    print("Guild does not exist")
         await self.close()
 
-client = MyClient()
+## PARSE ARGUMENTS
+parser = argparse.ArgumentParser()
+parser.add_argument("--show", help="output what would be backed up", action="store_true")
+parser.add_argument('guild', nargs='?', type=int)
+parser.add_argument('category', nargs='?', type=int)
+parser.add_argument('channel', nargs='?', type=int)
+args = parser.parse_args()
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+
+## READ IN API KEY
 try:
     with open("key.txt", "r") as f:
         content = f.read()
