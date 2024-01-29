83638cb0592e9570f79002ff4dba41dfd12920cf
Ben Williams <40040345+benw23@users.noreply.github.com>
2024-01-28 21:12:04 -0500

    plain text, single user option

--- main.py
+++ main.py
@@ -35,96 +35,60 @@ def printChannels(category):
 async def backupGuild(guild):
     print("backing up guild {} : {}".format(guild.id, guild))
 
-    folderName = os.path.join(ROOT_DIR, str(guild.id))
-    if not os.path.exists(folderName):
-        os.makedirs(folderName)
-
-    f = open(folderName+"/index.htm", 'w', encoding="utf-8")
-    f.write("<h1>"+guild.name+"</h1>\n<ul>\n")
+    f = args.o
 
     for category in guild.categories:
-        f.write("<li><a href='{}'>{}</a></li>\n<ul>\n".format(str(category.id)+"/index.htm", category.name))
-        for channel in category.channels:
-            f.write("<li><a href='{}'>{}</a></li>\n".format(str(category.id)+"/"+str(channel.id)+"/index.htm", channel.name))
-        f.write("</ul>\n")
-        f.close()
-        f = open(folderName+"/index.htm", 'a',  encoding="utf-8")
         await backupCategory(category)
 
-    f.write("</ul>")
-    f.close()
     return
 
 async def backupCategory(category):
     print("--- backing up category {} : {}".format(category.id, category))
 
-    rootFolder = os.path.join(ROOT_DIR, str(category.guild.id))
-    folderName = rootFolder+"/"+str(category.id)
-    if not os.path.exists(folderName):
-        os.makedirs(folderName)
-
-    f = open(folderName+"/index.htm", 'w', encoding="utf-8")
-    f.write(f"<h1>\n<a href='../index.htm'>{ category.guild.name }</a> / { category.name } \n</h1>\n\n<ul>\n")
+    f = args.o
 
     for channel in category.channels:
         if channel.type == discord.ChannelType.text:
-            f.write(f"<li><a href='{ channel.id }'>{ channel.name }</a></li>\n")
             await backupChannel(channel)
-
-    f.write("</ul>")
-    f.close()
+    
     return
 
 async def backupChannel(channel):
     print("------ backing up channel {} : {}".format(channel.id, channel))
 
-    rootFolder = os.path.join(ROOT_DIR, str(channel.guild.id))
-    folderName = rootFolder+"/"+str(channel.category.id)+"/"+str(channel.id)
-    if not os.path.exists(folderName):
-        os.makedirs(folderName)
-
-    f = open(folderName+"/index.htm", 'w', encoding="utf-8")
-
-    f.write("<h1>\n")
-    f.write(f"<a href='../../index.htm'>{ channel.guild.name }</a> / \n")
-    f.write(f"<a href='../index.htm'>{ channel.category.name }</a> / { channel.name }\n")
-    f.write("</h1>\n\n")
-
-
-    srcName = os.path.join(folderName, "src")
-    if not os.path.exists(srcName):
-        os.makedirs(srcName)
+    f = args.o
 
     out = []
     async for m in channel.history(limit=None):
         thisLine = ""
-        thisLine += f'<p>{ m.author.name }: { m.content }'
+        if (args.user) and (m.author.id == args.user):
+            thisLine += m.content.replace("\n", " ") + "\n"
+        elif not args.user:
+            thisLine += m.author.name + ": " + m.content + "\n"
 
-        for a in m.attachments:
-            r = requests.get(a.url, stream = True)
-            file_name = a.url.split('/')[-1]
-            ext = file_name.split('.')[-1]
+#        for a in m.attachments:
+#            r = requests.get(a.url, stream = True)
+#            file_name = a.url.split('/')[-1]
+#            ext = file_name.split('.')[-1]
 
             # if the file name already exists, append a random number
             # from 0-9 until the file does not exist
-            fullPath = os.path.join(srcName, file_name)
-            while os.path.exists(fullPath):
-                i = file_name.rfind('.')
-                file_name = file_name[:i] + str(random.randint(0,9)) + file_name[i:]
-                fullPath = os.path.join(srcName, file_name)
+#            fullPath = os.path.join(srcName, file_name)
+#            while os.path.exists(fullPath):
+#                i = file_name.rfind('.')
+#                file_name = file_name[:i] + str(random.randint(0,9)) + file_name[i:]
+#                fullPath = os.path.join(srcName, file_name)
 
-            with open(folderName+"/src/"+file_name,'wb') as out_file:
-                shutil.copyfileobj(r.raw, out_file)
-            print(a.url)
-            if (ext == 'png' or ext == 'jpg' or ext == 'jpeg' or ext == 'gif'):
-                thisLine += '<img src="src/'+file_name+'"></img>'
-            else:
-                thisLine += f'<a href="src/{ file_name }">{ file_name }</a>'
-        thisLine += '</p>\n'
+#            with open(folderName+"/src/"+file_name,'wb') as out_file:
+#                shutil.copyfileobj(r.raw, out_file)
+#            print(a.url)
+#            if (ext == 'png' or ext == 'jpg' or ext == 'jpeg' or ext == 'gif'):
+#                thisLine += '<img src="src/'+file_name+'"></img>'
+#            else:
+#                thisLine += f'<a href="src/{ file_name }">{ file_name }</a>'
         out.append(thisLine)
     for o in out[::-1]:
         f.write(o)
-    f.close()
 
     return
 
@@ -182,9 +146,11 @@ class MyClient(discord.Client):
 ## PARSE ARGUMENTS
 parser = argparse.ArgumentParser()
 parser.add_argument("--show", help="output what would be backed up", action="store_true")
-parser.add_argument('guild', nargs='?', type=int)
-parser.add_argument('category', nargs='?', type=int)
-parser.add_argument('channel', nargs='?', type=int)
+parser.add_argument("--user", "-u", help="user id (if you intend only to back up messages from a single user)", type=int)
+parser.add_argument('--guild', type=int)
+parser.add_argument('--category', type=int)
+parser.add_argument('--channel', type=int)
+parser.add_argument('-o', help="output file", type=argparse.FileType('w', encoding='utf-8'))
 args = parser.parse_args()
 
 intents = discord.Intents.default()
@@ -194,6 +160,11 @@ client = MyClient(intents=intents)
 if not args.guild and not args.show:
     print("Please specify a guild. Use -h or --help to see options.")
     quit()
+
+if not args.o and not args.show:
+    print("Please specify an output file. Use -h or --help to see options.")
+    quit()
+
 
 ## READ IN API KEY
 try:
